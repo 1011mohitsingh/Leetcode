ed57f88f87098f6b0e7b9f303fb68c0da28f48e4
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-09 19:18:01 +0530

    Time: 1307 ms (6.73%), Space: 55 MB (5.39%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,28 @@
-import java.util.*;
+class Solution{
+    public List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        HashSet<List<Integer>> hashSet = new HashSet<List<Integer>>();
 
-public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums); 
-        
-        int n = nums.length;
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            
-            int target = -nums[i];
-            int l = i + 1, r = n - 1;
-            
-            while (l < r) {
-                int sum = nums[l] + nums[r];
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    
-                    while (l < r && nums[l] == nums[l + 1]) l++;
-                    while (l < r && nums[r] == nums[r - 1]) r--;
-                    
-                    l++;
-                    r--;
-                } else if (sum < target) {
-                    l++;
-                } else {
-                    r--;
+        for(int i=0;i<nums.length;i++){
+            HashSet<Integer> innerHashSet = new HashSet<Integer>();
+            for(int j=i+1;j<nums.length;j++){
+                int k = -1*(nums[i]+nums[j]);
+                if(innerHashSet.contains(k) == false){
+                    innerHashSet.add(nums[j]);
+                } else{
+                    ArrayList<Integer> a = new ArrayList<Integer>();
+                    a.add(nums[i]);
+                    a.add(nums[j]);
+                    a.add(k);
+                    Collections.sort(a);
+                    if(hashSet.contains(a) == false){
+                        ans.add(a);
+
+                        hashSet.add(a);
+                    }
                 }
             }
         }
-        return result;
+        return ans;
     }
 }
