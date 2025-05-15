ee9783b17fa1f8485d6afecfd6c5a918337fcfe5
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-16 01:26:41 +0530

    Time: 6 ms (32.19%), Space: 46.6 MB (17.35%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,10 +1,32 @@
+// most optimized approach 
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int UniqueNumber = 0;
+
+//         for(int i=0;i<nums.length;i++){
+//             UniqueNumber = UniqueNumber ^ nums[i];
+//         }
+//         return UniqueNumber;
+//     }
+// }
+
+
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int UniqueNumber = 0;
+        Arrays.sort(nums);
 
-        for(int i=0;i<nums.length;i++){
-            UniqueNumber = UniqueNumber ^ nums[i];
+        if(nums[0] == nums.length ){
+            return nums[0];
         }
-        return UniqueNumber;
+
+        for(int i=0;i<nums.length-1;i+=2){
+            if(nums[i+1] > nums[i]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
     }
 }
+
