cea35eed67ad614d850c25bab57f5cf766283772
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-15 22:17:28 +0530

    Time: 0 ms (100%), Space: 45.4 MB (43.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,18 +1,33 @@
+// brute force approach 
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         int i=0;
+//         if(nums[0] != 0){
+//             return 0;
+//         }
+//         while(i<nums.length-1){
+//             if (nums[i+1]-nums[i] != 1){
+//                 return nums[i]+1;
+//             }
+//             i++;
+//         }
+//         return nums[i]+1;
+//     }
+// }
+
 class Solution {
-    public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        int i=0;
-        
-        if(nums[0] != 0){
-            return 0;
+    public int missingNumber(int[] nums){
+        int n = nums.length;
+        int expectedSum = 0;
+        int actualSum = 0;
+        for(int i=0;i<=n;i++){
+            expectedSum += i;
         }
 
-        while(i<nums.length-1){
-            if (nums[i+1]-nums[i] != 1){
-                return nums[i]+1;
-            }
-            i++;
-        }
-        return nums[i]+1;
+        for(int i=0;i<n;i++){
+            actualSum += nums[i];
+        }   
+        return expectedSum - actualSum;     
     }
 }
