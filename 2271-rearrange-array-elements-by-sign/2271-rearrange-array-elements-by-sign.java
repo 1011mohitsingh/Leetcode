da9f63ba7749b7097697bcd92eadcf91bc007122
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-20 14:15:04 +0530

    Time: 5 ms (32.55%), Space: 86.3 MB (5.53%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,22 +1,54 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> PList = new ArrayList<>();
-        ArrayList<Integer> NList = new ArrayList<>();
-        int[] result = new int[n];
+        // this is solved using the array list with complexity O(n)
+
+        // ArrayList<Integer> PList = new ArrayList<>();
+        // ArrayList<Integer> NList = new ArrayList<>();
+        // int[] result = new int[n];
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0){
+        //         PList.add(nums[i]);
+        //     } else {
+        //         NList.add(nums[i]);
+        //     }
+        // }
+
+        // for(int i=0;i<n/2;i++){
+        //     result[2*i] = PList.get(i);
+        //     result[2*i+1] = NList.get(i);
+        // }
+        // return result;
+
+
+        int k=0;
+        int l= 0;
+        int m = 0;
+        int o = 0;
+        int p=0;
+        int[] PArray = new int[n/2];
+        int[] NArray = new int[n/2];
+        int[] RArray = new int[n];
 
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                PList.add(nums[i]);
-            } else {
-                NList.add(nums[i]);
+            if(nums[i] >0 ){
+                PArray[k++] = nums[i];
+            } 
+            else {
+                NArray[l++] = nums[i];
             }
         }
-
+        
         for(int i=0;i<n/2;i++){
-            result[2*i] = PList.get(i);
-            result[2*i+1] = NList.get(i);
+            if(i<PArray.length){
+                RArray[p++] = PArray[m++];
+            }
+            if(i<NArray.length){
+                RArray[p++] = NArray[o++];
+            }
+            
         }
-        return result;
+        return RArray;
     }
 }
