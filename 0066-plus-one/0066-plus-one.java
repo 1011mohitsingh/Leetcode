fe113c3bfe0f8bfc51fcbd0f3e4d532d9bf2f156
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-29 09:22:07 +0530

    Time: 0 ms (100%), Space: 41.7 MB (82.48%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,42 @@
-import java.util.*;
-import java.math.BigInteger;
-
 class Solution {
     public int[] plusOne(int[] digits) {
-        BigInteger sum = BigInteger.ZERO;
-
-        for(int i=0;i<digits.length;i++){
-            sum = sum.multiply(BigInteger.TEN).add(BigInteger.valueOf(digits[i]));
+        int n = digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        sum = sum.add(BigInteger.ONE);
-        String numSum = sum.toString();
-        int [] result = new int[numSum.length()];
 
-        for(int i=0;i<numSum.length();i++){
-            result[i] = numSum.charAt(i) - '0';
-        }
+        int[] result = new int[n+1];
+        result[0] = 1;
         return result;
     }
+    
 }
+
+
+// 
+
+// import java.util.*;
+// import java.math.BigInteger;
+
+// class Solution {
+//     public int[] plusOne(int[] digits) {
+//         BigInteger sum = BigInteger.ZERO;
+
+//         for(int i=0;i<digits.length;i++){
+//             sum = sum.multiply(BigInteger.TEN).add(BigInteger.valueOf(digits[i]));
+//         }
+//         sum = sum.add(BigInteger.ONE);
+//         String numSum = sum.toString();
+//         int [] result = new int[numSum.length()];
+
+//         for(int i=0;i<numSum.length();i++){
+//             result[i] = numSum.charAt(i) - '0';
+//         }
+//         return result;
+//     }
+// }
+
