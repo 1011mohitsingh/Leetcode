414c17effcab8b5aeab7ad94fb68acfafe57e763
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-05-30 14:03:44 +0530

    Time: 15 ms (45.47%), Space: 58.2 MB (58.02%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,28 @@
+import java.util.HashMap;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        boolean flag = false;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                flag = true;
-                break;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num:nums){
+            if(map.containsKey(num)){
+                return true;
+            } else {
+                map.put(num,1);
             }
         }
-        return flag;
+        return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         boolean flag = false;
+//         Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i++){
+//             if(nums[i] == nums[i+1]){
+//                 flag = true;
+//                 break;
+//             }
+//         }
+//         return flag;
+//     }
+// }
