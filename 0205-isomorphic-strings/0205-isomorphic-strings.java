fa98bb7c2a05465b00a268857e7e1baf2a3292e0
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-07-16 16:36:47 +0530

    Time: 5 ms (97.87%), Space: 42.6 MB (36.53%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != s.length()){
-            return false;
-        }
+        if (s.length() != t.length()) return false;
 
-        Map<Character, Character> table = new HashMap<>();
-        for(int i=0;i<s.length();i++){
+        int[] mapST = new int[256]; 
+        int[] mapTS = new int[256];
+         
+        for (int i = 0; i < s.length(); i++) {
             char original = s.charAt(i);
             char replacement = t.charAt(i);
-
-            if(!table.containsKey(original)){
-                if(!table.containsValue(replacement)){
-                    table.put(original, replacement);
-                }else {
-                    return false;
-                }
+            
+            if (mapST[original] == 0 && mapTS[replacement] == 0) {
+                mapST[original] = replacement;
+                mapTS[replacement] = original;
             } else {
-                char mappedCharacter = table.get(original);
-                if(mappedCharacter != replacement){
+                if (mapST[original] != replacement || mapTS[replacement] != original) {
                     return false;
                 }
             }
@@ -25,3 +21,31 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         if(s.length() != s.length()){
+//             return false;
+//         }
+
+//         Map<Character, Character> table = new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             char original = s.charAt(i);
+//             char replacement = t.charAt(i);
+
+//             if(!table.containsKey(original)){
+//                 if(!table.containsValue(replacement)){
+//                     table.put(original, replacement);
+//                 }else {
+//                     return false;
+//                 }
+//             } else {
+//                 char mappedCharacter = table.get(original);
+//                 if(mappedCharacter != replacement){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
