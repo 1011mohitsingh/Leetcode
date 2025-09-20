dbe1af5a5af9bb2a9a4f6d039d8f0b1304f45f21
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-09-20 17:51:06 +0530

    Time: 4 ms (83.34%), Space: 60 MB (68.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,34 +1,76 @@
 class Solution {
+    public ListNode findMid(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> st = new Stack<>();
+        if(head == null || head.next == null) return true;
 
-        int count = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            count++;
-            temp = temp.next;
+        ListNode midNode = findMid(head);
+
+        ListNode prev = null;
+        ListNode curr = midNode;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
+        ListNode left = head;
+        ListNode right = prev;
 
-        temp = head;
-        for (int i = 0; i < count / 2; i++) {
-            st.push(temp.val);
-            temp = temp.next;
-        }
-
-        if (count % 2 != 0) {
-            temp = temp.next;
-        }
-
-        while (temp != null) {
-            if (st.pop() != temp.val) {
+        while(right != null){
+            if(left.val != right.val){
                 return false;
             }
-            temp = temp.next;
+            left = left.next;
+            right = right.next;
         }
-
         return true;
     }
 }
+
+
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//         Stack<Integer> st = new Stack<>();
+
+//         int count = 0;
+//         ListNode temp = head;
+//         while (temp != null) {
+//             count++;
+//             temp = temp.next;
+//         }
+
+//         temp = head;
+//         for (int i = 0; i < count / 2; i++) {
+//             st.push(temp.val);
+//             temp = temp.next;
+//         }
+
+//         if (count % 2 != 0) {
+//             temp = temp.next;
+//         }
+
+//         while (temp != null) {
+//             if (st.pop() != temp.val) {
+//                 return false;
+//             }
+//             temp = temp.next;
+//         }
+
+//         return true;
+//     }
+// }
 
 
 // class Solution {
