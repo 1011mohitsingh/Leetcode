ee77062478520d315ba93c0608c70077d8e9cf54
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-10-16 20:43:45 +0530

    Time: 3 ms (54.18%), Space: 44.4 MB (94.51%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        ArrayList<String> result = new ArrayList<>();
+        result.add(words[0]);
+
+        for(int i=1;i<words.length;i++){
+            if(!isAnagram(result.get(result.size()-1), words[i])){
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
+    public static boolean isAnagram(String a, String b){
+        if(a.length() != b.length()){
+            return false;
+        }
+        char[] arr1 = a.toCharArray();
+        char[] arr2 = b.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+    }
+}
