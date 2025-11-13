d24a66d1906d6c2010ea1d754dec2d5f04329867
MOHIT SINGH <155709797+1011mohitsingh@users.noreply.github.com>
2025-11-13 22:04:37 +0530

    Time: 7 ms (78.52%), Space: 47.6 MB (17.78%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,18 +1,18 @@
 class Solution {
     public int maxOperations(String s) {
-        long ans = 0;
-        int ones = 0;
-        int n = s.length();
+        long operations = 0;
+        int ones = 0, n = s.length();
 
-        for (int i = 0; i < n; ++i) {
-            if (s.charAt(i) == '1') {
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '1') {
                 ones++;
             } else { 
                 if (i == n - 1 || s.charAt(i + 1) == '1') {
-                    ans += ones;
+                    operations += ones;
                 }
             }
         }
-        return (int) ans;
+        return (int) operations;
     }
 }
